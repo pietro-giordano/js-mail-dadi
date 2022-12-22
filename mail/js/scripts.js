90fb8f6935eec59b4b322f9d5c9d1069cca63943
Pietro Giordano <giopi87@gmail.com>
2022-12-22 17:49:25 +0100

    esercizio mail completo

--- mail/js/scripts.js
+++ mail/js/scripts.js
@@ -9,14 +9,31 @@ const emailList = ['spiderman@gmail.com', 'thor@gmail.com', 'batman@gmail.com', 
 const userEmail = prompt('Inserisci email');
 console.log('Email inserita: ' + userEmail);
 
-if (emailList.includes(userEmail, 0) == true) {
+// if (emailList.includes(userEmail, 0) == true) {
 
-      console.log('Mail presente');
-      alert('La mail inserita è presente nel nostro database');
+//       console.log('Mail presente');
+//       alert('La mail inserita è presente nel nostro database');
 
-} else {
+// } else {
 
-      console.log('Email errata o non valida');
-      alert('La mail inserita è errata o non valida');
+//       console.log('Email errata o non valida');
+//       alert('La mail inserita è errata o non valida');
+
+// }
+
+for (let i = 0; i < emailList.length; i++) {
+
+      if (emailList[i] == userEmail) {
+
+            console.log('Email presente');
+            alert('Email presente');
+            break;
+
+      } else if (i == emailList.length - 1) {
+
+            console.log('Email errata o non valida');
+            alert('La mail inserita è errata o non valida');
+
+      }
 
 }
